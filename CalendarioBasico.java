6a008648c90154af1df533ebe0202697191861d3
Jorge Jaular <jordijaulas13@gmail.com>
2017-10-30 16:46:21 +0100

    Ya se han añadido los tres atributos, establecidos como day, month y year.

---
+++ CalendarioBasico.java
@@ -0,0 +1,39 @@
+
+/**
+ * La idea de este ejercicio es crear un calendario básico con el día, el mes
+ * y el año en el que nos encontremos, y modificarlo a nuestro antojo. Cada mes
+ * tendrá treinta días, todos y cada uno de los meses.
+ *
+ * @author (Jorge Jaular)
+ * @version (30-10-2017)
+ */
+public class CalendarioBasico
+{
+    // El día establecido en el calendario en valor numérico o entero.
+    private int day;
+    // El mes establecido en el calendario en valor numérico o entero.
+    private int month;
+    // El año establecido en el calendario en valor numérico o entero.
+    private int year;
+
+    /**
+     * Constructor for objects of class CalendarioBasico
+     */
+    public CalendarioBasico()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
